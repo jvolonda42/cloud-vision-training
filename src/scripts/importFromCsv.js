c6734eb8c7b53f4fe95639ce584e2b55b49bf647
jvolonda <jeremvol@gmail.com>
2018-04-01 17:24:23 +0200

    :recycle: importFromCsv is now an independant script.

--- src/scripts/importFromCsv.js
+++ src/scripts/importFromCsv.js
@@ -1,27 +1,54 @@
 var fs = require('fs');
 var parse = require('csv-parse');
+// var Product = require('../models/Product.model').default;
+import async from 'async';
 import Product from '../models/Product.model';
+import db from '../libs/database';
+
+const logAndExit = (message, statusCode) => {
+  console.log(message);
+  process.exit(statusCode);
+}
 
 var parser = parse({ delimiter: ';', columns: true }, function (err, data) {
-  // console.log(data[0])
-  data.map(d => {
-    Models.Product.findOne({ ref: d.id })
+  data.map((d, index) => {
+    Product.findOne({ ref: d.id })
       .then(product => {
-        if (!product)
-          new Product({
-            ref: d.id,
-            name: d.title,
-            type: d.sleeve,
-            imageUrl: `https:${d.photo}`,
-            pageUrl: d.url,
-          }).save();
+        if (!product) {
+          new Product({ ref: d.id, name: d.title, type: d.sleeve, imageUrl: `https:${d.photo}`, pageUrl: d.url, }).save()
+            .then(res => {
+              if (index === data.length - 1) {
+                logAndExit('products successfully imported', 0);
+              }
+            })
+            .catch(err => {
+              logAndExit('error while saving product in mongo', 1);
+            })
+        }
+        else {
+          logAndExit('product already registereds', 1);
+        }
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        logAndExit('error while checking if product already exist in mongo', 1);
+      });
   });
 });
 
 const parseCsv = (filePath) => {
-  fs.createReadStream(filePath).pipe(parser);
+  const stream = fs.createReadStream(filePath);
+  stream.pipe(parser);
+  stream.on('error', function () {
+    logAndExit('error with file', 1);
+  });
 }
 
-export default parseCsv;
+if (process.argv.length === 3) {
+  db.once('open', () => {
+    const fileName = process.argv[2];
+    parseCsv(fileName)
+  });
+}
+else {
+  logAndExit('missing file', 1);
+}
